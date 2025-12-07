556f7e67efbfe96636ed3a32417454493f35557b
Girish-Reddy-24 <girish244180@gmail.com>
2025-12-07 22:14:49 +0000

    Wire Cart component to CartProvider and add /cart route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes} from 'react-router-dom';
 import Header from './components/Header';
 import CardList from './components/CardList';
 import SingleView from './components/SingleView';
+import Cart from './components/Cart';
 import { CartProvider } from './state/CartProvider'
 function App() {
   
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<CardList />} />
           <Route path="/product/:id" element={<SingleView />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </CartProvider>
     </div>
